39e78882e040e7d7cf80dc61daae423a3c4484c3
AhmerAli17 <ahmerali1771@gmail.com>
2024-04-07 22:09:40 +0100

    first commit

---
+++ src/app/components/Navbar/navbar1.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { IoCall } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaRegClock } from "react-icons/fa6";
+import { MdFacebook } from "react-icons/md";
+import { AiFillInstagram } from "react-icons/ai";
+import { TbBrandBehance } from "react-icons/tb";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { RiYoutubeFill } from "react-icons/ri";
+import { BsPersonFill } from "react-icons/bs";
+import Link from 'next/link';
+const Navbar1 = () => {
+  return (
+    <div className='relative'>
+        <div className='bg-blue-600 py-3 flex justify-center gap-10  '>
+           <div className='text-white flex  gap-1 '>
+            <div className='space-y-5'><IoCall /></div>
+           <div className='text-xs'>+92 3455555396</div>
+           </div>
+           <div className='text-white flex gap-1'>
+            <div className='space-y-5'><FaLocationDot /></div>
+            <div className='text-xs'>Office #304-B Amna Plaza, Rawalpindi, Pakistan</div>
+            </div>  
+            <div  className='text-white flex gap-3'>
+              <div className='space-y-5'><FaRegClock /></div>
+              <div className='text-xs'>Mon - Fri 8.00 - 20.00</div>
+              </div> 
+              <div className='text-white flex gap-3'>
+                <div>
+                  <Link href={"https://www.facebook.com/ezitech"}>
+                  <MdFacebook />
+                  </Link>
+                  </div>
+                <div>
+                  <Link href={"https://www.instagram.com/ezitechpk/"}>
+                  <AiFillInstagram />
+                  </Link>
+                  </div>
+                <div>
+                  <Link href={"https://www.behance.net/ezitech"}>
+                  <TbBrandBehance />
+                  </Link>
+                  </div>
+                <div>
+                  <Link href={"https://pk.linkedin.com/company/ezitechpk"}>
+                  <TiSocialLinkedin />
+                  </Link>
+                  </div>
+                <div>
+                  <Link href={"https://www.youtube.com/channel/UCPJvS7gvIV9nm6paydinYWg"}>
+                  <RiYoutubeFill />
+                  </Link>
+                  </div>
+                </div>  
+                <div className='text-white flex gap-3' >
+                  <div className='space-y-5' ><BsPersonFill /> </div>
+                  <div className='flex gap-2' >
+                    <div className='text-xs'>Login</div>
+                    <div className='text-xs'>|</div>
+                    <div className='text-xs'>Register</div>
+                  </div>
+                  </div> 
+    </div>
+    </div>
+  )
+}
+
+export default Navbar1
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Navbar1 from './components/Navbar/navbar1'
+export default function Home() {        
+  return (
+    <div>
+      <Navbar1/>
+    </div>
+  )
+}
